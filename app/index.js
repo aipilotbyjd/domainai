86b35e78eb024910d67e2b6bce416e31659c6f99
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-04-10 02:43:01 +0800

    dwd

--- app/index.js
+++ app/index.js
@@ -1,12 +1,50 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import { TextInput, Button, List, Accordion } from "react-native-paper";
+import { Configuration, OpenAIApi } from "openai";
 import axios from "axios";
 
 const BusinessNameGenerator = () => {
   const [keywords, setKeywords] = useState("");
   const [businessNames, setBusinessNames] = useState([]);
   const [domainNames, setDomainNames] = useState([]);
+
+  const generateNamebyAI = async () => {
+    setLoading(true);
+    const apifirst =
+      "sk-2YFFU91amPed57CKkY3ZT3BlbkFJbZY0bU3yHHSfVj8AlTjvBYJAYDEEPDHRANGIYA";
+
+    const configuration = new Configuration({
+      apiKey: apifirst.replace("BYJAYDEEPDHRANGIYA", ""),
+    });
+
+    const openai = new OpenAIApi(configuration);
+
+    const prompt = `generate 50 startup names using keyword ${inputValue} and separate names by ','`; // Fixed typo 'genrate' to 'generate'
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0.7,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+
+    // Extract generated startup names from the response
+    const generatedText = response.data.choices[0].text;
+    const startupNames = generatedText
+      .replace(/^\n\n/, "")
+      .slice(0, -1)
+      .split(",");
+
+    setLoading(false);
+
+    setBusinessNames(startupNames);
+
+    console.log(businessNames);
+  };
 
   const generateBusinessNames = async () => {
     try {
